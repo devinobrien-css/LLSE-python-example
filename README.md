ee73b81fe0e5d129092fde3f4e148c94422067ee
Devin O'Brien <61803565+devinobrien-css@users.noreply.github.com>
2023-03-03 20:13:28 -0500

    finished

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# LLSE-python-example
+# CSI 536 HW 2 - LLSE Regression
+
 The following repository has an example of LLSE Regression in python
+
+# Execution Instructions
+
+### Install Dependencies
+
+- This application was built in python 3.11
+
+- Install `pipenv` by following (this documentation)[https://pypi.org/project/pipenv/]
+
+> Alternatively, you can install pipenv using brew following (this documentation)[https://formulae.brew.sh/formula/pipenv]
+
+- Once installed, change the current working directory to this project's root
+
+- Run `pipenv shell` to launch the virtual environment
+
+- Run `pipenv install` to install all required dependencies for this application
+
+### Executing the application
+
+- Once the dependencies have been installed, run `python3 hw2.py`
+
+- A pop-up will display with the plotted regression lines, and the coefficients and residuals will be printed to console
--- hw2.py
+++ hw2.py
@@ -1,22 +1,69 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
-
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from numpy.polynomial import polynomial as P
 
 def main():
+    X=[]
     x=[]
     y=[]
+
+    # import dataset
     with open('hw2.dat') as file:
         for row in file:
             data_in = row.split(' ')
+            X.append([float(data_in[0]),float(data_in[1])])
             x.append(float(data_in[0]))
             y.append(float(data_in[1]))
-    
-    plt.title('Degree 1')
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.scatter(x,y)
+
+    fig, axs = plt.subplots(2,2)
+
+    # degree 1
+    coef,resid,rank,sing,cond = np.polyfit(x, y, 1, full=True)
+    print(f'''DEGREE 1:
+-------------------------------------------
+coefficients/parameters: {coef}
+residual/optimal error: {resid}
+''')
+    axs[0,0].plot(x, y, '.', color="red")
+    axs[0,0].plot(x, np.polyval(coef, x))
+    axs[0,0].set_title('Polynomial order 1')
+
+    # degree 3
+    coef,resid,rank,sing,cond = np.polyfit(x, y, 3, full=True)
+    print(f'''DEGREE 3:
+-------------------------------------------
+coefficients/parameters: {coef}
+residual/optimal error: {resid}
+''')
+    axs[0,1].plot(x, y, '.', color="red")
+    axs[0,1].plot(x, np.polyval(coef, x))
+    axs[0,1].set_title('Polynomial order 3')
+
+    # degree 5
+    coef,resid,rank,sing,cond = np.polyfit(x, y, 5, full=True)
+    print(f'''DEGREE 5:
+-------------------------------------------
+coefficients/parameters: {coef}
+residual/optimal error: {resid}
+''')
+    axs[1,0].plot(x, y, '.', color="red")
+    axs[1,0].plot(x, np.polyval(coef, x))
+    axs[1,0].set_title('Polynomial order 5')
+
+    # degree 7
+    coef,resid,rank,sing,cond = np.polyfit(x, y, 7, full=True)
+    print(f'''DEGREE 7:
+-------------------------------------------
+coefficients/parameters: {coef}
+residual/optimal error: {resid}
+''')
+    axs[1,1].plot(x, y, '.', color="red")
+    axs[1,1].plot(x, np.polyval(coef, x))
+    axs[1,1].set_title('Polynomial order 7')
+
     plt.show()
 
 if __name__ == '__main__':
